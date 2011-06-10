896c1147305de7d2c2fbceb04fd6c8e6bb7932e4
tickleman <mc@thgbm.net>
2011-06-10 19:07:23 +0200

    [0.01] Empty new plugin

---
+++ src/fr/crafter/tickleman/RealMobSpawner/RealMobSpawnerPlugin.java
@@ -0,0 +1,32 @@
+package fr.crafter.tickleman.RealMobSpawner;
+
+import fr.crafter.tickleman.RealPlugin.RealPlugin;
+
+//############################################################################ RealMobSpawnerPlugin
+public class RealMobSpawnerPlugin extends RealPlugin
+{
+
+	//-------------------------------------------------------------------------- RealMobSpawnerPlugin
+	public RealMobSpawnerPlugin()
+	{
+		super("tickleman", "RealMobSpawner", "0.01");
+	}
+
+	//------------------------------------------------------------------------------------- onDisable
+	@Override
+	public void onDisable()
+	{
+		super.onDisable();
+	}
+
+	//-------------------------------------------------------------------------------------- onEnable
+	@Override
+	public void onEnable()
+	{
+		// events listeners
+		//PluginManager pm = getServer().getPluginManager();
+		// enable
+		super.onEnable();
+	}
+
+}
